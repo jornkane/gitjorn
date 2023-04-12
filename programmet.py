4b8a221fc10afc6c86d666e3a8be3faca57bc043
jornkane <jorn.kanestrom@mrfylke.no>
2023-04-12 11:56:03 +0200

    test på første committing...

---
+++ programmet.py
@@ -0,0 +1,8 @@
+print('Dette er programmet som vi skal utvikle i lag')
+print('Planen er å.....')
+
+
+# første commit:
+print('Hello git bash!')
+print('Git add, og git commit -m med kommentar i "hermetegn"')
+
