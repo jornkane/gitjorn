c056ff57cde594a26b653ad3bb8208ce18f8d4a2
jornkane <jornkane@gmail.com>
2026-01-21 13:57:35 +0100

    Overflytting fra python_grafer til funksjonsdrøfting.html er igang pga nedleggelse av trinket

--- python/testpyt.py
+++ python/testpyt.py
@@ -1,45 +1,24 @@
 import os as o
 o.system('cls' ) #De to første linjene er for å rydde terminal+
-# color themes - klikk på tannhjul og velg themes - color theme
-# For å for eksempel fikse kun på kommentarfargen:
-# ctrl+shit+og velg settings json og lim inn denne nederst
-# kommaet er komma du på legge til på slutten av forrige
-'''
-,
-    "editor.tokenColorCustomizations": {
-        "[Tomorrow Night Blue]": {
-            "comments": "#00c524"
-    }
-}
-'''
 
+import numpy as np
+import matplotlib.pyplot as plt
 
-'''
-import random as ra
-maxpoeng = ra.randint(1,100) # ra = random, randint= tilfeldig tall
-tall = ra.randint(0,maxpoeng) 
-print('Du har en prøve som gir totalt',maxpoeng,'poeng')
-print('Jeg fikk',tall,'poeng!')
-if tall < 20*maxpoeng/100:
-  print("Karakter: 1")
-elif tall <40*maxpoeng/100:
-  print('Karakter: 2')
-elif tall <60*maxpoeng/100:
-  print('Karakter: 3')
-elif tall<80*maxpoeng/100:
-  print('Karakter: 4')
-elif tall<93*maxpoeng/100:
-  print('Karakter: 5')
-else:
-  print('Gratulerer du fikk karakteren 6!')
-  '''
+x = np.arange(-3,3,0.5) # lager matteliste (array)
 
-print('På bygg 7  har vi 4 etasjer.\n'
-      'Hvor høyt er det opp til 4.?')
-trapper = 4
-trinnprtrapp = 19
-trinn = 0.2 # meter
+def f(x):
+  return x**2-1
 
-print('Det er:',trapper*trinnprtrapp*trinn)
+for tall in x:
+  if f(tall)==0:
+    print('f(x)=0 når x =',tall)
 
-
+print('--------------------')
+plt.plot(x, f(x)) # velger hva som skal vises i grafikkfeltet
+plt.title('Grafisk framstilling av f(x)=x**2-1') # tittel på grafen
+plt.xlabel('x-akse') # navn på x-aksen
+plt.ylabel('f(x)-akse') # navn på y-aksen
+plt.axhline(0, color="darkred", linewidth=1.5) # farge på x-aksen
+plt.axvline(0, color="forestgreen", linewidth=1.5) # farge på y-aksen
+plt.grid()
+plt.show() # viser den grafiske framstillingen
